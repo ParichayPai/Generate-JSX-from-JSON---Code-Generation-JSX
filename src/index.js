c2dc190a16c9158445f3fbc8a49b7b173be69d7d
ParichayPai <parichaypai393@gmail.com>
2020-10-03 20:25:40 +0530

    genCode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+
+type Node={
+    "type": "",
+    "name": "",
+    "style": {},
+    "children": [Node]
+}
+
 function generateCodeFromObject(obj){
     //return a code generated string
-   }
+    return React.createElement(
+        obj.name,
+        obj.style,
+        obj.children
+    )
+}
    
 module.exports=generateCodeFromObject;
 
